d15229de7728d20db5f1653192e25b70fa5e9e7d
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2025-08-17 02:39:29 +0700

    feat: add websocket composable

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import type { Log } from "@/types/log";
 
-const { fetch, pending } = useApi();
+const { fetch } = useApi();
+const { isConnected, subscribe } = useSocket("logger");
 
 const logs = ref<Log[]>([]);
 
@@ -9,6 +10,10 @@ const loggerFetch = async () => {
   const res = await fetch<Log[]>("/logs");
   logs.value = res?.data ?? [];
 };
+
+subscribe("log", async () => {
+  await loggerFetch();
+});
 
 onMounted(async () => {
   await loggerFetch();
@@ -20,6 +25,7 @@ onMounted(async () => {
     <div>
       <h2 class="text-sm font-medium text-gray-400">Overview</h2>
       <h1 class="text-2xl font-bold">Dashboard</h1>
+      <h2>{{ isConnected ? "Connected" : "Disconnected" }}</h2>
     </div>
     <div class="grid grid-cols-5 gap-4">
       <DashCard />
@@ -36,19 +42,8 @@ onMounted(async () => {
         <UButton>Clear log</UButton>
       </div>
       <div
-        :class="
-          pending
-            ? 'flex min-h-[50rem] w-full flex-col items-center justify-center bg-gray-800 transition-all'
-            : 'bg-default flex min-h-[50rem] w-full flex-col justify-start'
-        "
-        class="rounded-md border border-gray-500 p-4"
+        class="bg-default flex min-h-[50rem] w-full flex-col justify-start rounded-md border border-gray-500 p-4"
       >
-        <div v-if="pending">
-          class="flex flex-col items-center justify-center space-y-4
-          text-center"
-          <Icon name="tabler:loader-2" size="50" class="animate-spin" />
-          <span>Retrieving logs...</span>
-        </div>
         <div v-for="(item, index) in logs" :key="index" class="space-y-3">
           <span
             :class="
